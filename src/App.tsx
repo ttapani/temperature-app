a197a7df77d492ccce8c75c26bc79fee32097df7
Tomi Heino <tomi.heino@eng.tamk.fi>
2019-02-26 19:21:06 +0200

    Add redux-persist

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import { createMuiTheme } from '@material-ui/core/styles';
 import red from '@material-ui/core/colors/red';
 import { Provider } from 'react-redux';
-import { initStore } from './store/store';
+import initStore from './store/store';
 import TemperatureApp from './Containers/TemperatureApp';
+import { PersistGate } from 'redux-persist/integration/react'
 
 
 const theme = createMuiTheme({
@@ -30,8 +31,10 @@ class App extends Component<IProps> {
         return (
             <MuiThemeProvider theme={theme}>
                 <CssBaseline/>
-                <Provider store={store}>
-                    <TemperatureApp/>
+                <Provider store={store.store}>
+                    <PersistGate loading={null} persistor={store.persistor}>
+                        <TemperatureApp/>
+                    </PersistGate>
                 </Provider>
             </MuiThemeProvider>
             );
